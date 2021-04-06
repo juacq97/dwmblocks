5d4d64c4a54fbd96ccc5be73763cf1ce7c35928c
Juan Adrian Castro Quintana <juacq97@tutanota.com>
2021-04-06 12:35:10 -0600

    lets go guix

--- blocks.h
+++ blocks.h
@@ -14,9 +14,11 @@
 static Block blocks[] = {
 /*	pathu				pathc					interval	signal */
 
-//  { PATH("spotify-bar"),	NULL,					 5,		6},
-  { PATH("volume"),		PATH("volume_button"),	                 0,		4},	
+//  { PATH("weather"),            NULL,                                    3600,          4},
+  { PATH("spotify-bar"),	NULL,					 5,		6},
   { PATH("battery"),		NULL,		                       120,		2},
+  { PATH("volume"),		PATH("volume_button"),	                 0,		4},	
+
   { PATH("fecha"),		PATH("date_button"),			60,		3},
   { PATH("red"),	        PATH("red_button"),                     30,		5},
 //{ PATH("cpu-temp"),		NULL,		                        15,		7},
